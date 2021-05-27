9a4e580d018872d66c88d90e6079904b9f718fb6
jak01002 <jake_osgood@tjx.com>
2021-05-27 17:30:48 -0400

    Completed Module

--- src/app.js
+++ src/app.js
@@ -6,6 +6,9 @@ const data = require('./data.js');
 const accounts = data.accounts;
 const users = data.users;
 const writeJSON = data.writeJSON;
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+const { ap } = require('ramda');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,25 +21,8 @@ app.get('/', function (req, res){
     res.render('index', {title: 'Account Summary', accounts});
 });
 
-app.get('/savings', (req, res) => res.render('account', {account: accounts.savings}));
-app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
-app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
-
-app.get('/transfer', (req, res) => res.render('transfer'));
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance -= req.body.amount;
-    accounts[req.body.to].balance += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('transfer', {message: 'Transfer Completed'});
-});
-
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount);
-    writeJSON();
-    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => res.render('profile', {user: users[0] }));
 
