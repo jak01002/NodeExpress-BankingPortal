728ab1f24b532b01331bbfddfe3d67770e06e662
jak01002 <jake_osgood@tjx.com>
2021-05-27 15:05:54 -0400

    Completed Module

--- src/app.js
+++ src/app.js
@@ -8,8 +8,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
+const users = JSON.parse(userData);
+
 app.get('/', function (req, res){
-    res.render('index', {title: 'Index'});
+    res.render('index', {title: 'Account Summary', accounts});
 });
+
+app.get('/savings', (req, res) => res.render('account', {account: account.savings}));
+app.get('/checking', (req, res) => res.render('account', {account: account.checking}));
+app.get('/credit', (req, res) => res.render('account', {account: account.credit}));
 
 app.listen(3000, () => console.log('PS project running on port 3000!'));
